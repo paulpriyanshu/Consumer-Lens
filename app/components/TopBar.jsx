25e68a72b3b42b10d83f7873d9e8d45e5123a35e
Priyanshu Paul <priyanshupaul@Priyanshus-Laptop.local>
2024-09-05 22:57:03 +0530

    made some changes

--- app/components/TopBar.jsx
+++ app/components/TopBar.jsx
@@ -2,7 +2,7 @@
 const TopBar = () => {
     return (
       <div className="flex justify-between items-center p-4 border-b">
-        <h2 className="text-xl font-semibold">Marketing</h2>
+        <h2 className="text-3xl font-bold font-sans">Marketing</h2>
         <div className="flex items-center">
           <button className="px-4 py-2 bg-gray-200 rounded-md">Last 30 Days</button>
 
